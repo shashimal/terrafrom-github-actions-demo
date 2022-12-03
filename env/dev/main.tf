ccc7fbd8962ad7240e2c0fdf68f39f6874e93b3b
Duleendra Shashimal <duleendra@bambu.co>
2022-12-04 01:47:15 +0800

    Add a new bucket

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -9,3 +9,7 @@ resource "aws_s3_bucket" "my_new_bucket2" {
   bucket = "duleendra-new-bucket-dus0009"
 }
 
+resource "aws_s3_bucket" "my_new_bucket3" {
+
+  bucket = "duleendra-new-bucket-dus0ddd009"
+}
